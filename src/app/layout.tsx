d8ada6848cfdfc743ce612de0da4d83b1988fa79
Colby Fayock <fayock@gmail.com>
2024-03-08 16:52:00 -0500

    remove tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-import Providers from '@/app/providers';
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -19,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{ children }</Providers>
+        { children }
       </body>
     </html>
   )
